353aceb906fdaac77f622600f27cf9b822e305e8
Plamen Petkov <plamen.petkov@ITs-MacBook-Pro.local>
2020-03-21 20:36:18 +0200

    Application Done

---
+++ src/components/CardList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((item, index) => {
+    return (
+      <Card key={index} id={item.id} email={item.email} name={item.name} />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+
+export default CardList;
